09b77d6f34ae9ac8b92faa0b8032d197f46deccd
YongXin Shi <shiyongxin@aliyun.com>
2018-01-05 16:31:18 +0800

    webpack-simple

--- src/main.js
+++ src/main.js
@@ -1,28 +1,7 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
-import axios from 'axios'
+import App from './App.vue'
 
-import VueMaterial from 'vue-material'
-import 'vue-material/dist/vue-material.min.css'
-
-Vue.use(VueMaterial)
-
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
-var app3 = new Vue({
+new Vue({
   el: '#app',
-  router,
-  template: '<App/>',
-  components: { App },
-  methods: {
-    aa: function () {
-      alert('aa')
-    }
-  }
+  render: h => h(App)
 })
-
-app3.aa()
