96064fe3b858b6d710299467b150547471ed8fc4
YongXin Shi <shiyongxin@aliyun.com>
2018-01-06 10:57:32 +0800

    layout

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,80 @@
 <template>
-  <div id="app">
-    <mu-appbar title="Title">
-      <mu-icon-button icon="menu" slot="left"/>
-      <mu-flat-button label="expand_more" slot="right"/>
-      <mu-icon-button icon="expand_more" slot="right"/>
-    </mu-appbar>
-    <h1>{{ msg }}</h1>
+  <div class="layout">
+    <div class="header">
+      <div class="logo">
+        Muse-UI
+      </div>
+    </div>
+    <div class="content">
+      <div class="body">
+        <mu-sub-header>阳光</mu-sub-header>
+        <mu-content-block>
+          散落在指尖的阳光，我试着轻轻抓住光影的踪迹，它却在眉宇间投下一片淡淡的阴影。调皮的阳光掀动了四月的心帘，温暖如约的歌声渐起。似乎在诉说着，我也可以在漆黑的角落里，找到阴影背后的阳光，找到阳光与阴影奏出和谐的旋律。我要用一颗敏感赤诚的心迎接每一缕滑过指尖的阳光！
+        </mu-content-block>
 
+        <mu-card>
+          <mu-card-header title="Myron Avatar" subTitle="sub title">
+            <mu-avatar src="/images/uicon.jpg" slot="avatar"/>
+          </mu-card-header>
+          <mu-card-title title="Content Title" subTitle="Content Title"/>
+          <mu-card-text>
+            散落在指尖的阳光，我试着轻轻抓住光影的踪迹，它却在眉宇间投下一片淡淡的阴影。
+            调皮的阳光掀动了四月的心帘，温暖如约的歌声渐起。
+            似乎在诉说着，我也可以在漆黑的角落里，找到阴影背后的阳光，
+            找到阳光与阴影奏出和谐的旋律。我要用一颗敏感赤诚的心迎接每一缕滑过指尖的阳光！
+          </mu-card-text>
+        </mu-card>
+      </div>
+    </div>
+    <div class="footer">
+      Muse-UI ©2017 Created by Muse-UI
+    </div>
 
-    <mu-card>
-      <mu-card-header title="Myron Avatar" subTitle="sub title">
-        <mu-avatar src="/images/uicon.jpg" slot="avatar"/>
-      </mu-card-header>
-      <mu-card-title title="Content Title" subTitle="Content Title"/>
-      <mu-card-text>
-        散落在指尖的阳光，我试着轻轻抓住光影的踪迹，它却在眉宇间投下一片淡淡的阴影。
-        调皮的阳光掀动了四月的心帘，温暖如约的歌声渐起。
-        似乎在诉说着，我也可以在漆黑的角落里，找到阴影背后的阳光，
-        找到阳光与阴影奏出和谐的旋律。我要用一颗敏感赤诚的心迎接每一缕滑过指尖的阳光！
-      </mu-card-text>
-      <mu-card-actions>
-        <mu-flat-button label="Action 1"/>
-        <mu-flat-button label="Action 2"/>
-      </mu-card-actions>
-    </mu-card>
   </div>
 </template>
-
 <script>
 export default {
-  name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      activeTab: 'tab1'
     }
   },
-  tt () {
-    return {
-      aa: 'DDDDDDDDDDDDDDDD'
+  methods: {
+    handleTabChange (val) {
+      this.activeTab = val
     }
   }
 }
 </script>
+<style scoped>
+.layout{
+  background-color: rgb(236, 236, 236);
+}
 
-<style>
+.header{
+  background-color: #7e57c2;
+}
 
-a {
-  color: #42b983;
+.logo{
+  font-size: 24px;
+  color: white;
+  display: inline-block;
+  padding: 10px 20px;
+}
+
+.content{
+  width: 90%;
+  margin: 0 auto;
+}
+
+.body{
+  background-color: white;
+  border-radius: 5px;
+  min-height: 500px;
+}
+
+.footer{
+  padding: 20px 0;
+  text-align: center;
 }
 </style>
