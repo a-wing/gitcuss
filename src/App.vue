1b05db6a1baa68bfedeb41f2325df07be9903f7d
YongXin Shi <shiyongxin@aliyun.com>
2018-01-06 18:14:48 +0800

    ajax data

--- src/App.vue
+++ src/App.vue
@@ -2,46 +2,72 @@
   <div class="layout">
     <div class="header">
       <div class="logo">
-        Muse-UI
+      Gitcuss
       </div>
     </div>
     <div class="content">
       <div class="body">
+        <button v-on:click="getTopic"> </button>
         <mu-sub-header>阳光</mu-sub-header>
         <mu-content-block>
-          散落在指尖的阳光，我试着轻轻抓住光影的踪迹，它却在眉宇间投下一片淡淡的阴影。调皮的阳光掀动了四月的心帘，温暖如约的歌声渐起。似乎在诉说着，我也可以在漆黑的角落里，找到阴影背后的阳光，找到阳光与阴影奏出和谐的旋律。我要用一颗敏感赤诚的心迎接每一缕滑过指尖的阳光！
+          如果我有一天能见到JavaScript的开发者 我一定会fuckfuckfuckfuck ta
         </mu-content-block>
 
-        <mu-card>
-          <mu-card-header title="Myron Avatar" subTitle="sub title">
-            <mu-avatar src="/images/uicon.jpg" slot="avatar"/>
-          </mu-card-header>
-          <mu-card-title title="Content Title" subTitle="Content Title"/>
-          <mu-card-text>
-            散落在指尖的阳光，我试着轻轻抓住光影的踪迹，它却在眉宇间投下一片淡淡的阴影。
-            调皮的阳光掀动了四月的心帘，温暖如约的歌声渐起。
-            似乎在诉说着，我也可以在漆黑的角落里，找到阴影背后的阳光，
-            找到阳光与阴影奏出和谐的旋律。我要用一颗敏感赤诚的心迎接每一缕滑过指尖的阳光！
-          </mu-card-text>
-        </mu-card>
+        <div v-for="(item, index) in items">
+          <a v-bind:href="[item.html_url]">{{ item.home_url }}
+
+            <mu-card>
+              <mu-row gutter>
+                <mu-col width="100" tablet="30" desktop="20">
+                  <mu-card-header v-bind:title="item.user.login" v-bind:subTitle="item.updated_at">
+                    <mu-avatar size="80" v-bind:src="item.user.avatar_url" slot="avatar"/>
+                  </mu-card-header>
+                </mu-col>
+                <mu-col width="100" tablet="60" desktop="80">
+                  <mu-card-title v-bind:title="item.title" v-bind:subTitle="item.updated_at"/>
+                  <mu-card-text>{{ item.body }}</mu-card-text>
+                </mu-col>
+              </mu-row>
+            </mu-card>
+
+          </a>
+        </div>
+
       </div>
     </div>
     <div class="footer">
-      Muse-UI ©2017 Created by Muse-UI
+      Gitcuss ©2017 Created by a-wing
     </div>
 
   </div>
 </template>
 <script>
+let api_url = "https://api.github.com/repos/JRT-FOREVER/JRT-discuss/issues"
+
 export default {
   data () {
     return {
+      items: [],
+      aaa: "AAAAAAAAAAAAAAAAA",
       activeTab: 'tab1'
     }
   },
   methods: {
     handleTabChange (val) {
       this.activeTab = val
+    },
+    getTopic: function () {
+      //axios.get(api_url)
+      var _this = this
+      this.$ajax.get(api_url)
+      .then(function (response) {
+        //console.log(_this.aaa)
+        //console.log(response.data)
+        _this.items = response.data
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import axios from 'axios'
+Vue.prototype.$ajax = axios
+
 import MuseUI from 'muse-ui'
 import 'muse-ui/dist/muse-ui.css'
 Vue.use(MuseUI)
